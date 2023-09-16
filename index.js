22e0a72b363fd4addc7f3b914917a6f43f41b7c4
ahnaf-talukder <81837075+AhnafTalukder@users.noreply.github.com>
2023-09-16 12:54:05 -0400

    index.js

--- index.js
+++ index.js
@@ -4,51 +4,34 @@ require([
      "esri/views/MapView",
 
      "esri/Basemap",
-     "esri/layers/VectorTileLayer",
-     "esri/layers/TileLayer",
+     "esri/layers/FeatureLayer",
 
      "esri/widgets/Search"
 
-   ], function (esriConfig, Map, MapView, Basemap, VectorTileLayer, TileLayer, Search) {
+   ], function (esriConfig, Map, MapView, Search, FeatureLayer) {
 
 
-    //id: 424cd9fdc73f40b19666a47296645756
+
 
     
-     esriConfig.apiKey = "AAPK9cc4cc305f9845c2bb3083f5c7ee4eea5zPDXY6y9PbgylTNH4pxcaa7EEaomXZE26iNJULwJdcMBYEaYCov3Y8zKdicns-0";
+     esriConfig.apiKey = "";
      
  
- 
-  const vectorTileLayer = new VectorTileLayer({
-    portalItem: {
-      id: "6976148c11bd497d8624206f9ee03e30" // Forest and Parks Canvas
-    },
-    opacity: 0.75
-  });
 
-  const imageTileLayer = new TileLayer({
-    portalItem: {
-      id: "1b243539f4514b6ba35e7d995890db1d" // World Hillshade
-    }
-  });
 
-  const basemap = new Basemap({
-    baseLayers: [
-
-      imageTileLayer,
-      vectorTileLayer
-
-    ]
-  });
-
-  const map = new Map({
-    basemap: basemap,
-  });
+const myMap = new Map({
+      basemap: "streets-vector"
+    });
+  
+    const air_quality_layer = new FeatureLayer({
+      url: "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Air_Quality_PM25_Latest_Results/FeatureServer"
+    });
+    myMap.add(air_quality_layer);
 
 
   const view = new MapView({
     container: "viewDiv",
-    map: map,
+    map: myMap,
 
     center: [-100,40],
     zoom: 3
